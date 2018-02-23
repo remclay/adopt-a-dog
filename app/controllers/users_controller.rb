bbbe4d753db3c91aab83d5339fe21e513fe439c5
remclay <remmy.lauren@gmail.com>
2018-02-22 18:06:20 -0700

    Add users_controller actions (get and post) for login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,24 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/login' do
+    if Helpers.logged_in?(session)
+      redirect to '/dogs'
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+   if user && user.authenticate(params[:password])
+     session[:user_id] = user.id
+     redirect to "/tweets"
+   else
+     redirect to '/signup'
+   end
+  end
+
+
+
 end

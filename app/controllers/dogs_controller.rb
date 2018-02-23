5ad8864717377f680d15fc209e81fec5efad076e
remclay <remmy.lauren@gmail.com>
2018-02-23 10:46:40 -0700

    Build create_dog view and new get and post actions in DogsController

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -9,4 +9,20 @@ class DogsController < ApplicationController
     end
   end
 
+  get '/dogs/new' do
+    if Helpers.logged_in?(session)
+      @breeds = Dog.all.collect{|d| d.breed}.uniq
+      erb :'dogs/create_dog'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/dogs' do
+    if params[:name] != "" && params[:age] != ""
+      @dog = Dog.create(params[:dog])
+      @dog.user_id = session[:id]
+      @dog.save
+    end
+  end
 end

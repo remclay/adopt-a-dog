f773b5fb33f9794309181ac0f934f4a04e7b67f3
remclay <remmy.lauren@gmail.com>
2018-02-23 09:49:18 -0700

    Begin adding routes to DogsController, add dogs/dogs view

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,3 +1,12 @@
 class DogsController < ApplicationController
 
+  get '/dogs' do
+    if Helpers.logged_in?(session)
+      @dogs = Dog.all
+      erb :'dogs/dogs'
+    else
+      redirect to '/login'
+    end
+  end
+
 end

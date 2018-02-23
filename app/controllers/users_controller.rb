b37616fe9b6473b8fb82e83881cfe3af1acc92fd
remclay <remmy.lauren@gmail.com>
2018-02-22 18:01:19 -0700

    Begin adding paths for UsersController, create form for users/create_user view, amend method in Helpers class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
 class UsersController < ApplicationController
-  
+
+  get '/signup' do
+    if !Helpers.logged_in?(session)
+      erb :'users/create_user'
+    else
+     redirect to '/dogs'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] != "" && params[:email] != "" && params[:password] != ""
+      #use validation
+      @user = User.create(params)
+      session[:id] = @user.id
+      redirect to '/dogs'
+    else
+      #error message
+      redirect to '/signup'
+    end
+  end
+
 end
---
+++ app/helpers/helpers.rb
@@ -0,0 +1,10 @@
+class Helpers
+
+  def self.logged_in?(session)
+    !!current_user
+  end
+
+  def self.current_user(session)
+    @current_user ||= User.find_by_id(session[:id]) if session[:id]
+  end
+end

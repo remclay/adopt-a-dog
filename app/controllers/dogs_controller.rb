ce7c7e6154ab50fd1289f8001e6696824bd4deab
remclay <remmy.lauren@gmail.com>
2018-02-24 13:25:32 -0700

    Add links to view dogs posted by current user or log out in dogs/dogs view, and add instance variables to controllers to allow links.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -3,6 +3,7 @@ class DogsController < ApplicationController
   get '/dogs' do
     if Helpers.logged_in?(session)
       @dogs = Dog.all
+      @user = User.find_by_id(session[:id])
       erb :'dogs/dogs'
     else
       redirect to '/login'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect to "/tweets"
+      redirect to "/dogs"
     else
       redirect to '/signup'
     end

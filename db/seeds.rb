a5d0adec4226dd95b2b7d3549e46dd2de2c9d15e
remclay <remmy.lauren@gmail.com>
2018-03-02 17:06:00 -0700

    Amend seeds.rb data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,5 @@ husky = Breed.create(name: "Husky")
 
 # Dogs
 Dog.create({name: "Rover", age: "8", user_id: "#{peter.id}", breed_ids: ["#{husky.id}"], description: "Rover loves playing fetch and requires a lot of attention. He is a medium-high energy dog. He is available for adoption right away."})
-Dog.create({name: "Fido", age: "2.5", user_id: "#{peter.id}", description: "Comfortable around other dogs and most cats. Fido is still being trained, but can already sit, fetch and stay! Please email me for pictures or to arrange a time to meet this charming boy."})
+Dog.create({name: "Fido", age: "2.5", user_id: "#{peter.id}", breed_ids: ["#{retriever.id}"], description: "Comfortable around other dogs and most cats. Fido is still being trained, but can already sit, fetch and stay! Please email me for pictures or to arrange a time to meet this charming boy."})
 Dog.create({name: "Prinny", age: "6", user_id: "#{kate.id}", breed_ids: ["#{maltese.id}"], description: "She simply loves attention, will sit at your feet all day and does not require a lot of exercise."})

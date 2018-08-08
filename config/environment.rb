220e4092289915dba4c2300a02b067f10858662f
remclay <remmy.lauren@gmail.com>
2018-08-07 19:04:52 -0600

    Change development database back to sqlite3, test in browser using shotgun.

--- config/environment.rb
+++ config/environment.rb
@@ -2,42 +2,32 @@ require 'bundler/setup'
 Bundler.require
 
 configure :development do
-  ActiveRecord::Base.establish_connection(
-    adapter: 'postgresql',
-    database: 'adopt_a_dog_development'
-  )
-end
+  ENV['SINATRA_ENV'] ||= "development"
 
-configure :production do
   ActiveRecord::Base.establish_connection(
-    adapter: 'postgresql',
-    database: 'adopt_a_dog_production',
-    pool: 5
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
   )
 end
 
 # configure :development do
-#   ENV['SINATRA_ENV'] ||= "development"
-#
 #   ActiveRecord::Base.establish_connection(
-#     :adapter => "sqlite3",
-#     :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+#     adapter: 'postgresql',
+#     database: 'adopt_a_dog_development'
 #   )
 # end
-#
-# # :database => "db/neighborhood#{ENV['SINATRA_ENV']}.sqlite"
-#
-# configure :production do
-# 	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-#
-# 	ActiveRecord::Base.establish_connection(
-# 			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-# 			:host     => db.host,
-# 			:username => db.user,
-# 			:password => db.password,
-# 			:database => db.path[1..-1],
-# 			:encoding => 'utf8'
-# 	)
-# end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
 
 require_all 'app'

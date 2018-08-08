1ba3948a9b820cc07424b18db9c62a98d6a085b2
remclay <remmy.lauren@gmail.com>
2018-08-07 18:05:00 -0600

    Add additional test user to gemfile, add new breed and dog posted by user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,10 @@ john = User.create({username: "john@example.com", email: "john@example.com", pas
 maltese = Breed.create(name: "Maltese")
 retriever = Breed.create(name: "Golden retriever")
 husky = Breed.create(name: "Husky")
+pointer = Breed.create(name: "Pointer")
 
 # Dogs
 Dog.create({name: "Rover", age: "8", user_id: "#{peter.id}", breed_ids: ["#{husky.id}"], description: "Rover loves playing fetch and requires a lot of attention. He is a medium-high energy dog. He is available for adoption right away."})
 Dog.create({name: "Fido", age: "2.5", user_id: "#{peter.id}", breed_ids: ["#{retriever.id}"], description: "Comfortable around other dogs and most cats. Fido is still being trained, but can already sit, fetch and stay! Please email me for pictures or to arrange a time to meet this charming boy."})
 Dog.create({name: "Prinny", age: "6", user_id: "#{kate.id}", breed_ids: ["#{maltese.id}"], description: "She simply loves attention, will sit at your feet all day and does not require a lot of exercise."})
+Dog.create({name: "Rosie", age: "2", user_id: "#{john.id}", breed_ids: ["#{pointer.id}"], description: "Rosie loves eating ice cream and going for walks. She is a playful but well behaved dog, who is always happy to see you."})
